bcff6aa096e42ca64df5577ed847eb9b7f40e79f
james <jamesthomasbester@gmail.com>
2022-07-04 18:33:59 +1000

    github api

--- controller/pages.js
+++ controller/pages.js
@@ -1,7 +1,20 @@
 const router = require('express').Router()
+const path = require('path')
 
 router.get('/', (req, res) => {
     res.sendFile('index.html')
+})
+
+router.get('/home', (req, res) => {
+    res.sendFile('index.html')
+})
+
+router.get('/about', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public', 'about.html'))
+})
+
+router.get('/projects', (req, res) => {
+    res.sendFile('../projects.html')
 })
 
 module.exports = router;

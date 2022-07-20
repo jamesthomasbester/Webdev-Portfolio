72d96bc7a05e59262b74f1fa57f6a9ef00ea4d2d
james <jamesthomasbester@gmail.com>
2022-07-20 21:05:11 +1000

    minor changes to porjects page

--- client/src/components/Card.js
+++ client/src/components/Card.js
@@ -1,22 +1,14 @@
 import React from "react";
+import { useEffect,useState } from "react";
 
 const Card = (language, name, htmlURL, updatedAt, id) => {
-
+    const [names, setNames] = useState()
+    useEffect(() => {
+        setNames(name)
+    }, [name])
     return (
         <div className="card mb-3">
-            <div className="row g-0">
-                <div className="col-md-4 text-center">
-                <iframe title={name} src=""></iframe>
-                </div>
-                <div className="col-md-8">
-                    <div className="card-body">
-                        <h2>{name}</h2>
-                        <h2>test</h2>
-                        <a href={htmlURL} className="card-text">{htmlURL}</a>
-                        <p className="card-text"><small class="text-muted">{updatedAt}</small></p>
-                    </div>
-                </div>
-            </div>
+            <h1>{names}</h1>
         </div>
         )
 }
--- client/src/components/projects.js
+++ client/src/components/projects.js
@@ -1,24 +1,41 @@
 import React from "react";
 import { useEffect, useState } from "react";
-import Card from "./Card";
 
 const Projects = () => {
     const [repo, setRepo] = useState([])
+    const [dist, setDist] = useState([])
 
     useEffect(() => {
-        fetch('https://api.github.com/users/jamesthomasbester/repos')
-        .then(response => response.json())
-        .then(result => setRepo(result))
-    }, [])
+        const repoData = async () => {
+            await fetch('https://api.github.com/users/jamesthomasbester/repos')
+            .then(response => response.json())
+            .then(result => {
+                setRepo(result)
+            })
+        }
+        repoData()
 
+    }, [])
     console.log(repo)
     return (
         <div>
             {repo.map(repo => (
-                <div key={repo.id}>
-                {repo.name}
+                <div className="card mb-3">
+                    <div className={repo.language}>
+                        <h2>{repo.language}</h2>
+                    </div>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h3>{repo.name}</h3>
+                            <h5>{repo.description}</h5>
+                            <a href={repo.html_url} className="card-text">{repo.html_url}</a>
+                            <div className="details"> 
+                                <p className="card-text">last update: {repo.updated_at}</p>
+                                <p classname="card-text">created: {repo.created_at}</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                // <Card key={repo.id} id={repo.id} language={repo.language} name={repo.name} htmlURL={repo.html_url} updatedAt={repo.updatedAt} />
             ))}
         </div>
         )
